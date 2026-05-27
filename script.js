5fad8e6ab42afac7edbda6b3b53f7b27b0401da5
itaa6431-dot <itaa6431@gmail.com>
2026-05-27 18:18:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,267 @@
+const dataSiswa = [
+  {
+    nisn: "3146525710",
+    nomor: "T1-26-03-35-0073-0001-8",
+    nama: "MUHAMAD ROIHAN MAULIDA",
+    ttl: "Tegal, 7 Januari 2014",
+    matematika: "40.00",
+    indonesia: "73.33"
+  },
+  {
+    nisn: "0146637439",
+    nomor: "T1-26-03-35-0073-0002-7",
+    nama: "M. KHAFID ALIFAUZI",
+    ttl: "Tegal, 20 Juli 2014",
+    matematika: "53.33",
+    indonesia: "70.00"
+  },
+  {
+    nisn: "3142939858",
+    nomor: "T1-26-03-35-0073-0003-6",
+    nama: "HANIA SYAKIRA",
+    ttl: "Tegal, 26 April 2014",
+    matematika: "26.67",
+    indonesia: "70.00"
+  },
+  {
+    nisn: "0131884294",
+    nomor: "T1-26-03-35-0073-0004-5",
+    nama: "DAFI NABIL MUSYAFA",
+    ttl: "Tegal, 28 Juli 2013",
+    matematika: "46.67",
+    indonesia: "53.33"
+  },
+  {
+    nisn: "3133944890",
+    nomor: "T1-26-03-35-0073-0005-4",
+    nama: "SITI MUJILAH",
+    ttl: "Tegal, 14 September 2013",
+    matematika: "50.00",
+    indonesia: "50.00"
+  },
+  {
+    nisn: "3136618392",
+    nomor: "T1-26-03-35-0073-0006-3",
+    nama: "M. NUR FIKRI SAPUTRA",
+    ttl: "Tegal, 18 Oktober 2013",
+    matematika: "56.67",
+    indonesia: "70.00"
+  },
+  {
+    nisn: "3132924635",
+    nomor: "T1-26-03-35-0073-0007-2",
+    nama: "DEVI NURJANAH",
+    ttl: "Tegal, 29 Agustus 2013",
+    matematika: "63.33",
+    indonesia: "80.00"
+  },
+  {
+    nisn: "3140856607",
+    nomor: "T1-26-03-35-0073-0008-9",
+    nama: "IFFA ASTILA LABIBAH",
+    ttl: "Tegal, 23 Mei 2014",
+    matematika: "50.00",
+    indonesia: "80.00"
+  },
+  {
+    nisn: "3148009904",
+    nomor: "T1-26-03-35-0073-0009-8",
+    nama: "SHEZA MUZROKAH",
+    ttl: "Tegal, 20 Juli 2014",
+    matematika: "40.00",
+    indonesia: "60.00"
+  },
+  {
+    nisn: "0134200074",
+    nomor: "T1-26-03-35-0073-0010-7",
+    nama: "AKBAR SABARA",
+    ttl: "Tegal, 14 Mei 2013",
+    matematika: "20.00",
+    indonesia: "33.33"
+  },
+  {
+    nisn: "0137208136",
+    nomor: "T1-26-03-35-0073-0011-6",
+    nama: "FADHIL YAQDAN NASRULLAH",
+    ttl: "Tegal, 6 Februari 2013",
+    matematika: "56.67",
+    indonesia: "53.33"
+  },
+  {
+    nisn: "3143568164",
+    nomor: "T1-26-03-35-0073-0012-5",
+    nama: "KALILA RIFDA",
+    ttl: "Tegal, 14 September 2014",
+    matematika: "43.33",
+    indonesia: "66.67"
+  },
+  {
+    nisn: "3135774710",
+    nomor: "T1-26-03-35-0073-0013-4",
+    nama: "SITI OLIVIA",
+    ttl: "Tegal, 7 Oktober 2013",
+    matematika: "36.67",
+    indonesia: "43.33"
+  },
+  {
+    nisn: "3127310882",
+    nomor: "T1-26-03-35-0073-0014-3",
+    nama: "M. ADAM",
+    ttl: "Tegal, 5 April 2013",
+    matematika: "43.33",
+    indonesia: "66.67"
+  },
+  {
+    nisn: "3137929292",
+    nomor: "T1-26-03-35-0073-0015-2",
+    nama: "ZAHRA DEA AZAHWA",
+    ttl: "Tegal, 24 Juni 2013",
+    matematika: "26.67",
+    indonesia: "60.00"
+  },
+  {
+    nisn: "0146709127",
+    nomor: "T1-26-03-35-0073-0016-9",
+    nama: "KEISHA OKTAVIANI",
+    ttl: "Tegal, 13 Oktober 2014",
+    matematika: "56.67",
+    indonesia: "80.00"
+  },
+  {
+    nisn: "0134175255",
+    nomor: "T1-26-03-35-0073-0017-8",
+    nama: "M. ILHAM RIFALDHI",
+    ttl: "Tegal, 9 November 2013",
+    matematika: "56.67",
+    indonesia: "66.67"
+  },
+  {
+    nisn: "0111004530",
+    nomor: "T1-26-03-35-0073-0018-7",
+    nama: "SITI NUR AZIZAH",
+    ttl: "Tegal, 29 Desember 2011",
+    matematika: "40.00",
+    indonesia: "30.00"
+  },
+  {
+    nisn: "0141477568",
+    nomor: "T1-26-03-35-0073-0019-6",
+    nama: "BENING RATU ALISKA",
+    ttl: "Tegal, 4 Januari 2014",
+    matematika: "46.67",
+    indonesia: "76.67"
+  },
+  {
+    nisn: "3145184840",
+    nomor: "T1-26-03-35-0073-0020-5",
+    nama: "MUHAMAD AL FATIH FABRIZIO",
+    ttl: "Tegal, 25 April 2014",
+    matematika: "53.33",
+    indonesia: "76.67"
+  },
+  {
+    nisn: "3130905487",
+    nomor: "T1-26-03-35-0073-0021-4",
+    nama: "MUHAMAD JABARUDIN",
+    ttl: "Tegal, 25 Juli 2013",
+    matematika: "33.33",
+    indonesia: "66.67"
+  },
+  {
+    nisn: "0135542490",
+    nomor: "T1-26-03-35-0073-0022-3",
+    nama: "ZULAIKA II AZZAHRA",
+    ttl: "Tegal, 17 September 2013",
+    matematika: "73.33",
+    indonesia: "83.33"
+  },
+  {
+    nisn: "3139250821",
+    nomor: "T1-26-03-35-0073-0023-2",
+    nama: "DIFA ADITIA",
+    ttl: "Tegal, 1 Januari 2013",
+    matematika: "53.33",
+    indonesia: "73.33"
+  },
+  {
+    nisn: "3142599389",
+    nomor: "T1-26-03-35-0073-0024-9",
+    nama: "SITI JULIANA SHIDQIA",
+    ttl: "Tegal, 10 Juli 2014",
+    matematika: "46.67",
+    indonesia: "73.33"
+  },
+  {
+    nisn: "0147648633",
+    nomor: "T1-26-03-35-0073-0025-8",
+    nama: "KHALISA MEYSUN FIKRATUHA",
+    ttl: "Tegal, 6 Mei 2014",
+    matematika: "43.33",
+    indonesia: "56.67"
+  },
+  {
+    nisn: "0143030248",
+    nomor: "T1-26-03-35-0073-0026-7",
+    nama: "MUHAMAD AHNAF",
+    ttl: "Tegal, 11 Juli 2014",
+    matematika: "46.67",
+    indonesia: "66.67"
+  },
+  {
+    nisn: "0122732651",
+    nomor: "T1-26-03-35-0073-0027-6",
+    nama: "M. ASKAR MAULANA",
+    ttl: "Tegal, 5 Oktober 2012",
+    matematika: "43.33",
+    indonesia: "33.33"
+  },
+  {
+    nisn: "0135721411",
+    nomor: "T1-26-03-35-0073-0028-5",
+    nama: "ZIDNI DHIYA'UL ULUM",
+    ttl: "Tegal, 17 Agustus 2013",
+    matematika: "66.67",
+    indonesia: "73.33"
+  }
+];
+
+function cekData() {
+  const nisnInput = document.getElementById("nisn").value.trim();
+  const hasil = document.getElementById("hasil");
+
+  const siswa = dataSiswa.find(
+    s => s.nisn === nisnInput
+  );
+
+  if (siswa) {
+    hasil.innerHTML = `
+      <div class="card">
+        <h3>HASIL TES KEMAMPUAN AKADEMIK</h3>
+
+        <p><strong>Nama Lengkap:</strong> ${siswa.nama}</p>
+        <p><strong>NISN:</strong> ${siswa.nisn}</p>
+        <p><strong>Nomor Peserta:</strong> ${siswa.nomor}</p>
+        <p><strong>Tempat, Tanggal Lahir:</strong> ${siswa.ttl}</p>
+
+        <hr>
+
+        <p><strong>Nilai Matematika:</strong> ${siswa.matematika}</p>
+        <p><strong>Nilai Bahasa Indonesia:</strong> ${siswa.indonesia}</p>
+
+        <hr>
+
+        <p style="color:green;">
+          Data berhasil ditemukan
+        </p>
+      </div>
+    `;
+  } else {
+    hasil.innerHTML = `
+      <div class="card">
+        <p style="color:red;">
+          NISN tidak ditemukan.
+        </p>
+      </div>
+    `;
+  }
+}
